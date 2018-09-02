6adddba3f114af429aedbc86999c37a6aef36b7f
jgre28 <jgre28@student.monash.edu>
2018-09-02 15:18:33 +1000

    started messing with bootstrap with the table

--- tute6/displayClient.php
+++ tute6/displayClient.php
@@ -14,19 +14,17 @@
     $query= "SELECT * FROM client ORDER BY fName";
     $result = $conn->query($query);
     ?>
-<table border="1">
+<div class="container">
+    <h2>Client Table</h2>
+    <table class="table table-striped">
     <th>Client</th>
     <?php while ($row = mysqli_fetch_array($result))
     { ?>
         <tr>
             <td><?php echo $row["gName"]?></td>
             <td><?php echo $row["fName"]?></td>
-            <td><?php echo $row["unitNum"]?></td>
-            <td><?php echo $row["streetNum"]?></td>
-            <td><?php echo $row["street"]?></td>
-            <td><?php echo $row["suburb"]?></td>
-            <td><?php echo $row["state"]?></td>
-            <td><?php echo $row["postcode"]?></td>
+            <td><?php echo $row["unitNum"]." ".$row["streetNum"]." ".$row["street"].", ".$row["suburb"].", ".$row["state"].", ".$row["postcode"]?></td>
+
             <td><?php echo $row["email"]?></td>
             <td><?php echo $row["mobile"]?></td>
             <td><?php echo $row["mailingList"]?></td>
@@ -36,8 +34,17 @@
         </tr>
     <?php } ?>
 </table>
+</div>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
+<script
+        src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
+<script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<script
+        src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
 </body>
 
