973392b530cdd442c44b5d898fce1bbb05696c1a
jgre28 <jgre28@student.monash.edu>
2018-09-03 15:27:35 +1000

    created display property page for tute 7

---
+++ tute7/connection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordan
+ * Date: 3/09/2018
+ * Time: 1:21 PM
+ */
+
+$HOST = "130.194.7.82";
+$UName = "s25099159";
+$PWord = "monash00";
+$DB = "s25099159";
+?>
