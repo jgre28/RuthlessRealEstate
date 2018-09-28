b31f5409266024cf1878f246e8746f21181cafe0
jgre28 <jgre28@student.monash.edu>
2018-09-28 20:27:34 +1000

    delete property working

--- propertyModify.php
+++ propertyModify.php
@@ -31,6 +31,17 @@ function isChecked($var)
 
     return $checked;
 }
+
+?>
+<script language="javascript">
+function confirmDelete()
+{
+window.location='propertyModify.php?propertyID=<?php echo $_GET["propertyID"]; ?>&Action=ConfirmDelete';
+}
+</script>
+<?php
+
+
 include("connection.php");
 $conn = new mysqli($HOST, $UName, $PWord, $DB)
 or die("Couldn't log on to database");
@@ -143,11 +154,6 @@ switch($strAction)
             $query="DELETE FROM property_feature WHERE propertyID =".$_GET["propertyID"];
             $result = $conn->query(($query));
 
-            //foreach ($_POST["description"] as $desc)
-            //{
-              //  echo $desc."hi\n";
-
-            //}
 
 
             if (isset($_POST["check"]))
@@ -183,7 +189,88 @@ switch($strAction)
             header("Location: displayProperties.php");
         }
         break;
-}?>
+
+case "DELETE":
+
+    //get the types name to display rather than the ID of the type
+    $type= $row["propertyType"];
+    $typeName = mysqli_fetch_row($conn->query("SELECT typeName FROM type WHERE $type= typeID"));
+
+    //formats the address to be nice
+    $address= "";
+    if (!empty($row["unitNum"])){
+    $address= $address."U".$row["unitNum"].", ";
+    }
+    $address= $address.$row["streetNum"]." ".$row["street"].", ".$row["suburb"].", ".$row["state"].", ".$row["postcode"];
+
+
+    //gets the sellers name
+    $sellerID=  $row["sellerID"];
+    $sellerName= mysqli_fetch_row($conn->query("SELECT gName, fName FROM client WHERE $sellerID= clientID"));
+    ?>
+<div class="container">
+    <h1>Confirm deletion of the following property record</h1>
+
+    <table>
+        <tr>
+            <th>Listing Date:</th>
+            <td><?php echo date("d/m/Y",strtotime($row["listingDate"]))?></td>
+        </tr>
+        <tr>
+            <th>Listing Price:</th>
+            <td><?php echo "$".$row["listingPrice"]?></td>
+        </tr>
+        <tr>
+            <th>Property Type:</th>
+            <td><?php echo $typeName[0]?></td>
+        </tr>
+        <tr>
+            <th>Address:</th>
+            <td><?php echo $address?></td>
+        </tr>
+        <tr>
+            <th>Seller:</th>
+            <td><?php echo $sellerName[0]." ".$sellerName[1]?></td>
+        </tr>
+        <tr>
+            <th>Description:</th>
+            <td><?php echo $row["description"]?></td>
+        </tr>
+
+    </table>
+    <br/>
+    <table align="center">
+        <tr>
+            <td><input type="button" value="Confirm" OnClick="confirmDelete();">
+            <td><input type="button" value="Cancel" OnClick="window.location='displayProperties.php'"></td>
+        </tr>
+    </table>
+</div>
+    <?php
+    break;
+
+case "ConfirmDelete":
+$query="DELETE FROM property WHERE propertyID =".$_GET["propertyID"];
+if($conn->query($query))
+{
+?>
+    <div class="container">
+    <h4>The property record has been successfully deleted</h4>
+
+        <?php
+    }
+    else {
+        echo "<center>Error Contacting Database<p /></center>";
+    }
+?>
+    <input type = "button" value="Return to List" OnClick="window.location='displayProperties.php'">
+    </div>
+
+<?php
+break;
+
+    } ?>
+
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
